da76844ee1f6430edc1529831973ec9baf5a417a
Pooja J N <pooja.j.n@ibm.com>
2023-07-17 13:52:23 +0530

    cleaning code

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -19,29 +19,3 @@ export class ProductListComponent implements OnInit {
       });
   }
 }
-//https://dummyjson.com/products?_limit
-// import { Component, OnInit } from '@angular/core';
-// import { ProductService } from '../product.service';
-
-
-// @Component({
-//   selector: 'app-product-list',
-//   templateUrl: './product-list.component.html',
-//   styleUrls: ['./product-list.component.scss']
-// })
-// export class ProductListComponent implements OnInit {
-//   products: any;
-
-//   constructor(private productService: ProductService) { }
-
-//   ngOnInit() {
-//     this.productService.getProducts().subscribe(
-//       (data: any[]) => {
-//         this.products = data.slice(0, 50); 
-//       },
-//       error => {
-//         console.log('An error occurred:', error);
-//       }
-//     );
-//   }
-// }
